c83e419332aeb18e3918a2c089e8b3587fb9c51e
Yanling Chen <cyl129@163.com>
2023-05-04 00:37:42 +0200

    tidy up files

--- data_gti.sh
+++
@@ -1,4 +0,0 @@
-tabgtigen table=rate_EMOS1.fits expression="RATE<=0.4912940252907291" gtiset=EMOS1_gti.fits
-tabgtigen table=rate_EMOS2.fits expression="RATE<=0.6046980368236037" gtiset=EMOS2_gti.fits
-tabgtigen table=rate_EPN.fits expression="RATE<=1.014412960048878" gtiset=EPN_gti.fits
-tabgtigen table=rate_Oot.fits expression="RATE<=0.9478405908978429" gtiset=Oot_gti.fits
